a5aa866f2b0d5cf5affc83869ded97d1794e99e7
vhamet <valentin.hamet@gmail.com>
2022-06-24 18:11:59 +0200

    tuning

--- js/carousel.js
+++ js/carousel.js
@@ -1,63 +1,62 @@
-let scrollAmount = 0
-let container, chevronContainerRight, chevronContainerLeft, scrollMax, scrollWidth
+let scrollAmount = 0;
+let container,
+  chevronContainerRight,
+  chevronContainerLeft,
+  scrollMax,
+  scrollWidth;
 
 const handleScrollLeft = () => {
-  if (chevronContainerLeft.classList.contains("disabled")) return
+  if (chevronContainerLeft.classList.contains("disabled")) return;
 
-  scrollAmount -= scrollWidth
-  chevronContainerRight.classList.remove("disabled")
-  if (scrollAmount <= 0)
-    chevronContainerLeft.classList.add("disabled")
+  scrollAmount -= scrollWidth;
+  chevronContainerRight.classList.remove("disabled");
+  if (scrollAmount <= 0) chevronContainerLeft.classList.add("disabled");
 
   container.scrollTo({
     top: 0,
     left: Math.max(scrollAmount, 0),
-    behavior: 'smooth'
-  })
-}
+    behavior: "smooth",
+  });
+};
 
 const handleScrollRight = () => {
-  if (chevronContainerRight.classList.contains("disabled")) return
+  if (chevronContainerRight.classList.contains("disabled")) return;
 
-  scrollAmount += scrollWidth
-  chevronContainerLeft.classList.remove("disabled")
+  scrollAmount += scrollWidth;
+  chevronContainerLeft.classList.remove("disabled");
   if (scrollAmount >= scrollMax)
-    chevronContainerRight.classList.add("disabled")
+    chevronContainerRight.classList.add("disabled");
 
   container.scrollTo({
     top: 0,
     left: Math.min(scrollAmount, scrollMax),
-    behavior: 'smooth'
-  })
-}
+    behavior: "smooth",
+  });
+};
 
 const handleContainerScroll = () => {
   if (container.scrollLeft >= scrollMax)
-    chevronContainerRight.classList.add("disabled")
-  else
-    chevronContainerRight.classList.remove("disabled")
+    chevronContainerRight.classList.add("disabled");
+  else chevronContainerRight.classList.remove("disabled");
 
-  if (container.scrollLeft <= 0)
-    chevronContainerLeft.classList.add("disabled")
-  else
-    chevronContainerLeft.classList.remove("disabled")
+  if (container.scrollLeft <= 0) chevronContainerLeft.classList.add("disabled");
+  else chevronContainerLeft.classList.remove("disabled");
 
-  scrollAmount = container.scrollLeft
-}
+  scrollAmount = container.scrollLeft;
+};
 
 document.addEventListener("DOMContentLoaded", function () {
-  container = document.querySelector(".reviews__container")
-  for (const review of reviews)
-    container.appendChild(createReviewCard(review))
+  container = document.querySelector(".reviews__container");
+  for (const review of reviews) container.appendChild(createReviewCard(review));
 
-  scrollMax = container.scrollWidth - container.clientWidth
-  scrollWidth = document.querySelector(".review-card").offsetWidth
+  scrollMax = container.scrollWidth - container.clientWidth;
+  scrollWidth = document.querySelector(".review-card").offsetWidth + 30;
 
-  chevronContainerLeft = document.querySelector(".chevron-container-left")
-  chevronContainerLeft.addEventListener("click", handleScrollLeft)
+  chevronContainerLeft = document.querySelector(".chevron-container-left");
+  chevronContainerLeft.addEventListener("click", handleScrollLeft);
 
-  chevronContainerRight = document.querySelector(".chevron-container-right")
-  chevronContainerRight.addEventListener("click", handleScrollRight)
+  chevronContainerRight = document.querySelector(".chevron-container-right");
+  chevronContainerRight.addEventListener("click", handleScrollRight);
 
-  container.addEventListener("scroll", handleContainerScroll)
-})
+  container.addEventListener("scroll", handleContainerScroll);
+});
